cfcb1535a67dc502709a05b2b8e5ab43642a2e74
heather-anne <heather@hiilite.com>
2017-12-27 11:55:00 -0800

    Hii Infinite Carousel

--- includes/shortcodes/hii_infinite_carousel.php
+++ includes/shortcodes/hii_infinite_carousel.php
@@ -12,61 +12,7 @@
 
 function add_hii_infinite_carousel_shortcode( $atts ){
 
-$el_class = $css = '';
-$shortcode_type =  $this->getShortcode();
-$atts = vc_map_get_attributes( $shortcode_type, $atts );
-$this->resetVariables( $atts, $content );
-extract( $atts );
 
-$this->setGlobalTtaInfo();
-$this->enqueueTtaStyles();
-$this->enqueueTtaScript();
-
-
-// It is required to be before tabs-list-top/left/bottom/right for tabs/tours
-$prepareContent = $this->getTemplateVariable( 'content' );
-
-$class_to_filter = $this->getTtaGeneralClasses();
-$class_to_filter .= vc_shortcode_custom_css_class( $css, ' ' ) . $this->getExtraClass( $el_class );
-
-
-
-$slider_type = '';
-$wrapper_attributes = array();
-$css_classes = array(
-	'slider',
-	$atts['slider_type'],
-);
-if($atts['slider_full_height'] == true){
-	$css_classes[] = 'slider_full_height';
-}
-$css_class = preg_replace( '/\s+/', ' ', apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, implode( ' ', array_filter( $css_classes ) ), $this->settings['base'], $atts ) );
-
-
-if(isset($atts['shape']) && $atts['shape'] != '')
-{
-	$wrapper_attributes[] = 'class="' . esc_attr( trim( $css_class ) ) . ' ' . $atts['shape'] . ' ' . $atts['c_align'] . '"';
-}
-else
-{
-	$wrapper_attributes[] = 'class="' . esc_attr( trim( $css_class ) ) . '"';
-}
-
-
-$output = '<hii-infinite-carousel ' . implode( ' ', $wrapper_attributes ) . '
-				  layout="responsive"
-				  width="'.$atts['slider_width'].'"
-				  height="'.$atts['slider_height'].'"
-				  style="width:'.$atts['slider_width'].'px;
-				  	height:'.$atts['slider_height'].'px;"
-				  type="slides"';
-$output .= ($atts['autoplay'] != 'none')?' autoplay delay="'.$atts['autoplay'].'000">':'>';
-if(isset($atts['title']) && $atts['title'] != '')
-{
-	$output .= '<strong>'.$atts['title'].'</strong>';
-}
-$output .= $prepareContent;
-$output .= '</hii-infinite-carousel>';
 
 
 
