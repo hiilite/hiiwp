baa828916f2fc73f088593dfeeda66b2ff102b4a
heather-anne <heather@hiilite.com>
2017-12-27 12:06:36 -0800

    Hii Infinite Carousel

--- includes/shortcodes/hii_infinite_carousel.php
+++ includes/shortcodes/hii_infinite_carousel.php
@@ -13,40 +13,6 @@
 function add_hii_infinite_carousel_shortcode( $atts ){
 
 
-$is_vc = (class_exists('Vc_Manager'))?true:false;
-/*extract( shortcode_atts( array(
-      'post_ids' => null,
-      'show_title'	=> true,
-      'show_excerpt'	=> true,
-      'show_btn'	=> true,
-      'btn_text' => __('Read More', 'hiiwp'),
-      'css' => '',
-    ), $atts ) );*/
-    
-$isPageEditable = vc_is_page_editable();
-
-$output = '';
-
-$output .= '<div class="' . esc_attr( $this->getElementClasses() ) . '"';
-$output .= ' id="' . esc_attr( $this->getTemplateVariable( 'tab_id' ) ) . '"';
-$output .= ' data-vc-content=".vc_tta-panel-body">';
-$output .= '<div class="vc_tta-panel-heading">';
-$output .= $this->getTemplateVariable( 'heading' );
-$output .= '</div>';
-$output .= '<div class="vc_tta-panel-body ' . $show . '">';
-if ( $isPageEditable ) {
-	$output .= '<div data-js-panel-body>'; // fix for fe - shortcodes container, not required in b.e.
-}
-$output .= $this->getTemplateVariable( 'content' );
-if ( $isPageEditable ) {
-	$output .= '</div>';
-}
-$output .= '</div>';
-$output .= '</div>';
-
-
-echo $output;
-
 
 
 }
